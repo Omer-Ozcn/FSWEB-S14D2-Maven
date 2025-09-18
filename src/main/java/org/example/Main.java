875a07ef339487c287e4788439c22146d6893a00
Omer-Ozcn <omerzcn@outlook.com>
2025-09-19 02:28:46 +0300

    Dosyalar tamamlandı ve testler başarılı

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,41 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.LampType;
+import org.example.model.enums.PaintColor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Wall w1 = new Wall("North");
+        Wall w2 = new Wall("South");
+        Wall w3 = new Wall("East");
+        Wall w4 = new Wall("West");
+        Ceiling ceiling = new Ceiling(270, PaintColor.WHITE);
+        Bed bed = new Bed("Modern", 2, 60, 1, 1);
+        Lamp lamp = new Lamp(LampType.NEON, true, 5);
+        Wardrobe wardrobe = new Wardrobe(200, 220, 85.5);
+        Carpet carpet = new Carpet(300, 400, PaintColor.GREEN);
+
+        Bedroom bedroom = new Bedroom(
+                "Master",
+                w1, w2, w3, w4,
+                ceiling,
+                bed,
+                lamp,
+                wardrobe,
+                carpet
+        );
+
+        w1.create(); w2.create(); w3.create(); w4.create();
+        bedroom.getCeiling().create();
+        bedroom.getBed().make();
+        bedroom.getLamp().turnOn();
+        bedroom.getWardrobe().add();
+        bedroom.getCarpet().lying();
+
+        System.out.println("Bedroom name: " + bedroom.getName());
+        System.out.println("Lamp style: " + bedroom.getLamp().getStyle());
+        System.out.println("Ceiling color: " + bedroom.getCeiling().getColor());
     }
+
 }
